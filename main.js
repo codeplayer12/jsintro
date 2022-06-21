b6b71779b9df1d74f58a2833e82138b1896cf781
noble mutoko <noblemutoko@gmail.com>
2022-06-21 21:26:11 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,3 @@
+document.getElementById('name').innerHTML = ' Name: Noble'
+document.getElementById('height').innerHTML = 'Height: 1.6m'
+document.getElementById('country').innerHTML = 'Country: Zimbabwe'
